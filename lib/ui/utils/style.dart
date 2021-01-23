28d9ee730657666ff3abba361d8d80b7e9b3e5e1
Mulyono Putra <mulyonoputra1994@gmail.com>
2021-01-23 15:13:20 +0700

    covid-19 app

---
+++ lib/ui/utils/style.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+final titleStyle = TextStyle(
+  color: Color(0xFF5B16D0),
+  fontFamily: 'Raleway',
+  fontSize: 26.0,
+  height: 1.5,
+  fontWeight: FontWeight.bold
+);
+
+final subtitleStyle = TextStyle(
+  color: Color(0xFF5B16D0),
+  fontSize: 18.0,
+  height: 1.2,
+  fontFamily: 'Raleway',
+);
